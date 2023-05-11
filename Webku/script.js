7d59b00046fa9efb2cfa1a10efb1cdf5fdbe1ee0
fatihalhijri <fatihalhijri02@gmail.com>
2023-05-11 15:06:18 +0700

    ganti nama

--- Webku/script.js
+++ Webku/script.js
@@ -3,9 +3,26 @@ function Menu(e) {
     e.name === 'menu' ? (e.name = "close", list.classList.add('top-[104px]'), 
     list.classList.add('opacity-100')) : (e.name = "menu" , list.classList.remove('top-[104px]'),
     list.classList.remove('opacity-100'))
+}
 
     // let list2 = document.querySelector('ul');
     // e.name === 'menu' ? (e.name = "close", list2.classList.add('bg-gray-900')) :
     // (e.name = "menu" , list2.classList.remove('bg-gray-900'))
 
-}
+    // window.onscroll = function () { xmenu() };
+   
+    // function xmenu() {
+    //     console.log("asa");
+    //     if (document.documentElement.scrollTop > 20) {
+    //         document.getElementById('header').classList.add('bg-white');
+    //         documnet.getElementById('header').classList.add('shadow-md');
+    //     } else {
+    //         document.getElementById('header').classList.remove('bg-white');
+    //         documnet.getElementById('header').classList.remove('shadow-md');
+    //     }
+    // }
+
+
+
+    
+
