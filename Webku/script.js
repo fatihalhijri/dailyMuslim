c4c8be69235a158ac4899da2c7e3b2a0aee7f73e
fatihalhijri <fatihalhijri02@gmail.com>
2023-05-14 07:24:43 +0700

    up to date

--- Webku/script.js
+++ Webku/script.js
@@ -1,59 +1,88 @@
 function Menu(e) {
     let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[104px]'), 
-    list.classList.add('opacity-100')) : (e.name = "menu" , list.classList.remove('top-[104px]'),
-    list.classList.remove('opacity-100'))
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[104px]'),
+        list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[104px]'),
+            list.classList.remove('opacity-100'))
 
 }
 
-    // let list2 = document.querySelector('ul');
-    // e.name === 'menu' ? (e.name = "close", list2.classList.add('bg-gray-900')) :
-    // (e.name = "menu" , list2.classList.remove('bg-gray-900'))
+// let list2 = document.querySelector('ul');
+// e.name === 'menu' ? (e.name = "close", list2.classList.add('bg-gray-900')) :
+// (e.name = "menu" , list2.classList.remove('bg-gray-900'))
 
-    // window.onscroll = function () { xmenu() };
-   
-    // function xmenu() {
-    //     console.log("asa");
-    //     if (document.documentElement.scrollTop > 20) {
-    //         document.getElementById('header').classList.add('bg-white');
-    //         documnet.getElementById('header').classList.add('shadow-md');
-    //     } else {
-    //         document.getElementById('header').classList.remove('bg-white');
-    //         documnet.getElementById('header').classList.remove('shadow-md');
-    //     }
-    // }
+// window.onscroll = function () { xmenu() };
 
-    window.onscroll = function () { menu() };
-    // var xstatus = false;
+// function xmenu() {
+//     console.log("asa");
+//     if (document.documentElement.scrollTop > 20) {
+//         document.getElementById('header').classList.add('bg-white');
+//         documnet.getElementById('header').classList.add('shadow-md');
+//     } else {
+//         document.getElementById('header').classList.remove('bg-white');
+//         documnet.getElementById('header').classList.remove('shadow-md');
+//     }
+// }
 
-    function menu() {
-        if (document.documentElement.scrollTop > 40) {
-            document.getElementById("header").classList.add("bg-transparent");
-            document.getElementById("header").classList.add("shadow-xl");
-            document.getElementById("header").classList.add("rounded-br-3xl");
-            document.getElementById("header").classList.add("rounded-bl-3xl");
-        } else {
-            document.getElementById("header").classList.remove("bg-transparent");
-            document.getElementById("header").classList.remove("shadow-xl");
-            document.getElementById("header").classList.remove("rounded-br-3xl");
-            document.getElementById("header").classList.remove("rounded-bl-3xl");
-        }
+window.onscroll = function () { menu() };
+// var xstatus = false;
+
+function menu() {
+    if (document.documentElement.scrollTop > 40) {
+        document.getElementById("header").classList.add("bg-transparent");
+        document.getElementById("header").classList.add("shadow-xl");
+        document.getElementById("header").classList.add("rounded-br-3xl");
+        document.getElementById("header").classList.add("rounded-bl-3xl");
+
+        document.getElementById("menu2").classList.add("bg-transparent");
+        document.getElementById("menu2").classList.add("md:shadow-none");
+        document.getElementById("menu2").classList.add("shadow-xl");
+        document.getElementById("menu2").classList.add("rounded-br-3xl");
+        document.getElementById("menu2").classList.add("rounded-bl-3xl");
+    } else {
+        document.getElementById("header").classList.remove("bg-transparent");
+        document.getElementById("header").classList.remove("md:shadow-none");
+        document.getElementById("header").classList.remove("shadow-xl");
+        document.getElementById("header").classList.remove("rounded-br-3xl");
+        document.getElementById("header").classList.remove("rounded-bl-3xl");
+
+        document.getElementById("menu2").classList.remove("bg-transparent");
+        document.getElementById("menu2").classList.remove("shadow-xl");
+        document.getElementById("menu2").classList.remove("rounded-br-3xl");
+        document.getElementById("menu2").classList.remove("rounded-bl-3xl");
     }
+}
 
-    // function showHideMenu() {
-    //     var menu = document.getElementById("daftarMenu");
-    //     if (xstatus) {
-    //         menu.classList.add("hidden");
-    //         menu.classList.remove("flex");
-    //         xstatus = false;
-    //     } else {
-    //         menu.classList.remove("hidden");
-    //         menu.classList.add("flex");
-    //         xstatus = true;
-    //     }
-    //     console.log(xstatus);
-    // }
+currentSlideID = 1;
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
 
 
-    
+function next() {
+    if (currentSlideID < totalSlides){
+        currentSlideID++;
+        showSlide()
+    }
+        
+}
+function prev() {
+    if (currentSlideID > 1){
+        currentSlideID--;
+        showSlide()
+    }
+        
+}
+
+function showSlide( ){
+    slides = document.getElementById('slider').getElementsByTagName('li')
+    for (let index = 0; index < totalSlides; index++){
+        const element = slides[index];
+        if(currentSlideID===index+1){
+            
+                element.classList.remove('hidden')
+            }
+            else{
+                element.classList.add('hidden')
+            }
+    }
+}
 
